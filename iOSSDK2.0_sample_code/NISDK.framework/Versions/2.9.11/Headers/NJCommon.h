bb73c45414207934a598b9403dd279f7978c8dd7
Louis Park <louispark@neolab.kr>
2018-01-29 11:59:49 +1100

    add NISDK v2.9.11 framework to sample code

---
+++ iOSSDK2.0_sample_code/NISDK.framework/Versions/2.9.11/Headers/NJCommon.h
@@ -0,0 +1,23 @@
+//
+//  NJCommon.h
+//  NISDK
+//
+//  Created by Ken You on 20/09/2014.
+//  Copyright (c) 2017 Neolab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "configure.h"
+
+extern NSString * const NJBTRegResultNotification;
+extern NSString * const NJBTDisconnectedNotification;
+extern NSString * const NJPenRegistrationNotification;
+extern NSString * const NJPenCommManagerWriteStartNotification;
+extern NSString * const NJPenCommParserPenReadyNotification;
+extern NSString * const NJPenCommParserPenPasswordSutupSuccess;
+extern NSString * const NJNoteBookPageDocumentOpenedNotification;
+extern NSString * const NJSettingsPenPasswordSetupCompleteNotificaiton;
+extern NSString * const NJCanvasOpenedReadyNotification;
+extern NSString * const NJPenCommManagerPenConnectionStatusChangeNotification;
+extern NSString * const NJPenCommParserPageChangedNotification;
+extern NSString * const NJPenCommParserPenPasswordValidationFail;
